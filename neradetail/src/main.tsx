f58108c0b7fdfde9532e61488901ff10101c2fd8
Adam Blahout <blahout.adam@seznam.cz>
2025-10-14 11:38:32 +0200

    fix css

--- neradetail/src/main.tsx
+++ neradetail/src/main.tsx
@@ -1,11 +1,11 @@
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import { BrowserRouter } from "react-router-dom";
+import { MantineProvider } from "@mantine/core";
+import { theme } from "./theme.ts";
 import App from "./App.tsx";
 import "@mantine/core/styles.css";
 import "./app.css";
-import { MantineProvider } from "@mantine/core";
-import { theme } from "./theme.ts";
 
 createRoot(document.getElementById("root")!).render(
   <StrictMode>
